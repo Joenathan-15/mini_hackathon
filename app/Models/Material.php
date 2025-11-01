d0cffa28430dd2ece735705f216426fae40a3e8c
AIZOFan <als.irfan28@gmail.com>
2025-11-01 08:20:38 +0700

    database

---
+++ app/Models/Material.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Material extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'file_name', 'title', 'description', 'price'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function details(): HasMany {
+        return $this->hasMany(MaterialDetail::class);
+    }
+
+    public function activities(): HasMany {
+        return $this->hasMany(Activity::class);
+    }
+}
---
+++ app/Models/MaterialDetail.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class MaterialDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['material_id', 'category_id'];
+
+    public function material(): BelongsTo {
+        return $this->belongsTo(Material::class);
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+}
